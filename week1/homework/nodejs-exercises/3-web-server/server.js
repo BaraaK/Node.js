4b69b16f2370eb407baf40a5e607543f42f4923d
Baraa Kolly <baraa.ali.kolly@gmail.com>
2020-05-29 15:04:27 +0200

    updated 3-web-server

--- week1/homework/nodejs-exercises/3-web-server/server.js
+++ week1/homework/nodejs-exercises/3-web-server/server.js
@@ -1,36 +1,38 @@
-var http = require('http');
+const http = require('http');
 const fs = require('fs')
+const path = require('path')
+
 
 
 //create a server
-let server = http.createServer(function (req, res) {
-    let path = req.url
-    if(path === '/') {
-        fs.readFile('index.html', (error , data)=>{  
-            if (error)
-                return console.log(error)
-          res.setHeader('Content-Type', 'text/html');
-          res.end(data) // Ends the response
-        }); 
+let server = http.createServer(function (req, res) { 
+    // file path
+    let filePath = path.join(__dirname, req.url == '/' ? 'index.html' : req.url)
+    // extension of file
+    let ext = path.extname(filePath)
+    let contenType = 'text/html' //initialize content-type
+    switch(ext) {
+        case '.js' :
+            contenType = 'text/javascript'
+            break
+        case '.css' :
+            contenType = 'text/css'
+            break                
     }
-    else
-        if(path === '/script.js') {
-            fs.readFile('script.js', (error , data)=>{  
-                if (error)
-                    return console.log(error)
-            res.setHeader('Content-Type', 'text/javaScript');
-            res.end(data) // Ends the response
-            }); 
+    fs.readFile(filePath, (error , data)=>{  
+        if (error) {
+            //ENOENT: no such file or directory
+            if (error.code == 'ENOENT') {
+                res.write('Page Not Found!')
+                res.end() 
+            }
+            else {
+                res.write(`There is an Error: ${error.code}`)
+                res.end()
+            }
         }
-    else
-        fs.readFile('style.css', (error , data)=>{  
-            if (error)
-                return console.log(error)
-          res.setHeader('Content-Type', 'text/css');
-          res.end(data) // Ends the response
-        }); 
-    
-  
-});
-
-server.listen(3000); // The server listens on port 3000
+        res.setHeader('Content-Type', contenType);
+        res.end(data)
+    });
+ 
+}).listen(3000); // The server listens on port 3000
